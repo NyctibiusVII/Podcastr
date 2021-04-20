24cc439c57d67304bb1ecf5e157f3948b454abf0
NyctibiusVII <matheus.dev.07@gmail.com>
2021-04-20 13:39:11 -0300

    fixed error renaming import

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import Head   from 'next/head'
-import styles from '../styles/pages/home.module.css'
+import styles from '../styles/pages/home.module.scss'
 
 export default function Home(props) {
     return (
