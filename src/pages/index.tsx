655b98faf5231b466583ce50c993430f6058e01a
NyctibiusVII <matheus.dev.07@gmail.com>
2021-05-24 22:18:13 -0300

    (°ー°〃)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -151,7 +151,7 @@ export const getStaticProps: GetStaticProps = async () => {
         params: {
             _limit: 12,
             _sort: 'published_at',
-            _order: 'desc',
+            _order: 'desc'
         }
     })
 
